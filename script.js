a2246d0f615c6a6d97cfcc8a4e9d475f5bd4ce51
Robert Tilton <robtilton2086@gmail.com>
2021-02-05 14:46:59 -0500

    defining more variables and setting up arrays

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var upperLetters = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split()];
+var lowerLetters = ['abcdefghijklmnopqrstuvwxyz'.split()];
+var numbers = ['0123456789'.split()];
+var specialChars = ['!@#$%^&*()_+-=`~?/.>,<'.split()];
 
 // Write password to the #password input
 function writePassword() {
+  var confirmUpper = confirm("Would you like to use uppercase letters?");
+  var confirmLower = confirm("Would you like to use lowercase letters?");
+  var confirmNum = confirm("Would you like to use numbers?");
+  var confirmSpec = confirm("Would you like to use special characters?")
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
