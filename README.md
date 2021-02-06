d58cf40db7c18156861194170fa87db42d7031b6
Robert Tilton <robtilton2086@gmail.com>
2021-02-06 12:21:40 -0500

    final changes to code, including comments and spacing, as well as an updated README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # JS-Password-Generator
+
+For this assignment, I built a random password generator along a series of pre-defined parameters. Mainly in focus were the ideas of character type submissions and length requirements, both of which are functional in the final product. Speaking of, the deployed project can be located [here](https://roberttilton.github.io/JS-Password-Generator/).
--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,7 @@ function generatePassword() {
     alert('Password length must be between 8 and 128 characters/a number.');
     return 'Password length must be between 8 and 128 characters/a number.';
   }
-  // Next we will prompt them for what characters they want. Will be a confirm prompt for all 4.
+  // Next we will prompt them for what characters they want.
   var confirmUpper = confirm("Would you like to use uppercase letters?");
   if (confirmUpper == true) {
     choices = choices + upperLetters
@@ -42,22 +42,21 @@ function generatePassword() {
   } else {
     choices = choices
   }
+
   // need to make sure that the user picked at least one character type for password.
   if (!confirmUpper && !confirmLower && !confirmNum && !confirmSpec) {
     alert('You need to pick at least one character type');
     return;
   }
-//After we have user inputs, we want to write a function to randomize the letters inside that we can give the password  (will use math.random to generate that).
-// Then we will creat another function- make an array w the results, an array for possible characters, 
-// and set an array for guarenteed characters (characters guarenteed for pw)
-// then we will create a loop that goes through the results array- (need to look up push and join)
-// Once we push into a new array (results array), using join we will take the characters from results array and turn into a string
+  
+  //Write a function to randomize what characters are chosen, taking in the choices previously.
+  // Created a new variable in order to store the result, then joined the array.
   var password = [];
   for (var i = 0; i < passLength; i++) {
     var choicesTogether = choices[Math.floor(Math.random() * choices.length)]
     password.push(choicesTogether);
   }
-  return(password.join(""));
+  return (password.join(""));
 }
 
 // After we turn into a string, we write the password to the page using(.value)
